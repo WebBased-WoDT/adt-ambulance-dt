425927964b8bbe49e82ed743bebabfb08b108c62
andrea.giulianelli <andrygiulio@gmail.com>
2023-12-05 09:00:42 +0100

    chore: update LICENSE

--- src/test/kotlin/TestApp.kt
+++ src/test/kotlin/TestApp.kt
@@ -1,13 +1,21 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.ints.shouldBeExactly
-
-/*
- * Copyright (c) 2023. Smart Operating Block
- *
- * Use of this source code is governed by an MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
 
 class TestApp : StringSpec({
     "2+2 is equal to 4" {

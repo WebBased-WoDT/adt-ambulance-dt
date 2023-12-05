3c2c2e46ac9c2ed567cf8d34d3523b5c3973d0b8
andrea.giulianelli <andrygiulio@gmail.com>
2023-12-05 18:14:00 +0100

    chore: add azure signalr event model

---
+++ src/main/kotlin/infrastructure/component/adtpresentation/ADTPresentation.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package infrastructure.component.adtpresentation
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonPrimitive
+
+/**
+ * It models the event received from Azure SignalR.
+ * It contains:
+ * - [dtId]
+ * - [eventType]
+ * - [eventDateTime]
+ * - a list of the current [properties]
+ * - a list of the current [relationships].
+ */
+@Serializable
+data class DigitalTwinUpdate(
+    val dtId: String,
+    val eventType: String,
+    val eventDateTime: String,
+    val properties: Map<String, JsonPrimitive> = mapOf(),
+    val relationships: List<DigitalTwinRelationship> = listOf(),
+)
+
+/**
+ * It models a relationship returned in the event from SignalR.
+ * Each relationship has a [sourceId] associated with a [targetId] by a [relationshipName].
+ */
+@Serializable
+data class DigitalTwinRelationship(
+    @SerialName("\$sourceId")
+    val sourceId: String,
+    @SerialName("\$relationshipName")
+    val relationshipName: String,
+    @SerialName("\$targetId")
+    val targetId: String,
+)

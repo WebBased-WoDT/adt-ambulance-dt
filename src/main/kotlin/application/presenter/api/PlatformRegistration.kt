b29b8c0eda171d070fe14edf018f034c2aca8617
andrea.giulianelli <andrygiulio@gmail.com>
2023-12-09 11:37:18 +0100

    chore: create platform registration payload

---
+++ src/main/kotlin/application/presenter/api/PlatformRegistration.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package application.presenter.api
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Presenter class to be able to deserialize platform registration data from the API.
+ * It contains the [self] field where the WoDT Digital Twins Platform send its URL.
+ */
+@Serializable
+data class PlatformRegistration(val self: String)
